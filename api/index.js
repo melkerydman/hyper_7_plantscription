61d39efdee7fcdbac0624e8706a72d9eee328207
Harry-Yates <hyates1@gmail.com>
2022-04-11 14:09:55 +0200

    Set up the server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,24 @@
-import express from "express"
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const connectDatabase = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URL, {});
+        console.log('Mongo db connected');
+    } catch (error) {
+        console.log(`Error:${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectDatabase();
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running, ${PORT}`);
+});
