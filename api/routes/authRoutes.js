3182cd2e9c1f9a83534c8a8b54dce1bb97f27640
AlexTeklu <alexanderosteklu@gmail.com>
2022-04-14 12:13:15 +0200

    auth login first trial

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/userModel.js';
+import CryptoJS from 'crypto-js';
+import jwt from 'jsonwebtoken';
 
 async function registerUser(req, res) {
     try {
@@ -21,11 +23,43 @@ async function registerUser(req, res) {
     }
 }
 
+async function userLogin(req, res) {
+    try {
+        const user = await User.findOne({
+            username: req.body.username,
+        });
+        !user && res.status(401).json('first error');
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        OriginalPassword !== req.body.password &&
+            res.status(401).json('wrong Credentials');
+
+        // const accessToken = jwt.sign(
+        //     {
+        //         id: user._id,
+        //         isAdmin: user.isAdmin,
+        //     },
+        //     process.env.JWT_SEC,
+        //     { expiresIn: '3d' }
+        // );
+        // const { password, others } = user._doc;
+
+        // res.status(200).json({ ...others, accessToken });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
 // Routes
 const authRoutes = express.Router();
 
 authRoutes.post('/', registerUser);
 // Routes to be created
-// authRoutes.post("/login", loginUser);
+authRoutes.post('/login', userLogin);
 
 export default authRoutes;
