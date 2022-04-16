f893360798f19087cc9255f37633a36c381b3038
AlexTeklu <alexanderosteklu@gmail.com>
2022-04-16 21:20:26 +0200

    userLogin is being created and finished

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -8,7 +8,8 @@ async function registerUser(req, res) {
         const newUser = await User.create({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: CryptoJS.AES.encrypt (
+                req.body.password, process.env.PASS_SEC,).toString(),
         });
 
         res.status(200).json({
@@ -23,43 +24,43 @@ async function registerUser(req, res) {
     }
 }
 
-async function userLogin(req, res) {
+ async function userLogin(req, res) {
     try {
         const user = await User.findOne({
-            username: req.body.username,
+            username: req.body.username
         });
-        !user && res.status(401).json('first error');
+           !user && res.status(401).json('first error wronge inpute');
 
         const hashedPassword = CryptoJS.AES.decrypt(
             user.password,
             process.env.PASS_SEC
+         );
+         const DefinedPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        DefinedPassword !== req.body.password &&
+        res.status(401).json('password does not match');        
+/////////JWT is created to make our registeration and login process more secure////
+// comment to be deleted later
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: '3d' }
         );
-        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        OriginalPassword !== req.body.password &&
-            res.status(401).json('wrong Credentials');
-
-        // const accessToken = jwt.sign(
-        //     {
-        //         id: user._id,
-        //         isAdmin: user.isAdmin,
-        //     },
-        //     process.env.JWT_SEC,
-        //     { expiresIn: '3d' }
-        // );
-        // const { password, others } = user._doc;
-
-        // res.status(200).json({ ...others, accessToken });
+          const { password, ...others } = user._doc;
+         res.status(200).json({...others, accessToken});
     } catch (error) {
         res.status(500).json(error);
     }
-}
+ }
 
 // Routes
 const authRoutes = express.Router();
-
+/////RegisterUser is being created /////
 authRoutes.post('/', registerUser);
-// Routes to be created
+//Login userAuth is created//////
 authRoutes.post('/login', userLogin);
 
 export default authRoutes;
