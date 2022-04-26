fda8592b6b04be86ca13b96b5de61735990c4f01
AlexTeklu <alexanderosteklu@gmail.com>
2022-04-26 11:05:43 +0200

    first functionality is created for products

--- client/src/pages/ProductsPage/index.jsx
+++ client/src/pages/ProductsPage/index.jsx
@@ -3,20 +3,34 @@
 import { Main } from './styled';
 import { Products } from '../../ProductData';
 import Product from './product';
-// import { useState, } from 'react';
+import { useEffect, useState, } from 'react';
 
 
 
  const AllProducts = () => {
-//   //  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-//      const getAllProducts = async () =>{
+    //  const getAllProducts = async () =>{
       
-//         const res = await fetch('http://localhost:8080/products');
-//         console.log(res);         
+    //     const res = await fetch('http://localhost:8080/products');
+    //     console.log(res);         
       
-//      }
-//      getAllProducts();
+    //  }
+    //  getAllProducts();
+
+    useEffect(() => {
+      const getProducts = async () => {
+        try {
+          const res = await fetch('http://localhost:8080/products' );
+          const data = await res.json();
+         
+           console.log(data);
+          // setProducts(data);
+        } catch (err) {}
+      };
+      getProducts();
+    }, []);
+    
 
 
     return (
